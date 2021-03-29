08591c2aa31f5c89623f13c3046ae056c98d2f54
wanghao <1782418372@qq.com>
2021-03-29 15:37:44 +0800

    222

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-#use Auth;
 use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
@@ -19,12 +18,19 @@ class SessionsController extends Controller
            'password' => 'required'
        ]);
 
-       if (Auth::attempt($credentials)) {
+       if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
            return redirect()->route('users.show', [Auth::user()]);
        } else {
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back()->withInput();
        }
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
     }
 }
